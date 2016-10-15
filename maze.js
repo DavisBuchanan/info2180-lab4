5b7feaa973354af16bdf8abc5d5ce0105fd4cb4f
DavisBuchanan <davis_buchanan@hotmail.com>
2016-10-14 23:23:30 -0500

    Add files via upload
    Exercise 1

---
+++ maze.js
@@ -0,0 +1,7 @@
+//Exercise 1
+window.onload = function() {
+	function hitWall() {
+		document.getElementById("boundary1").className = "boundary youlose";
+	}
+	document.getElementById("boundary1").addEventListener("mouseover",hitWall);
+}
